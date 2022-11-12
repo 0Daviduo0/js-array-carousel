e5eb67ff9d515531339adef58f26567190385de3
Daviduo <davide0400@gmail.com>
2022-11-12 18:48:06 +0100

    post to html successful

--- js/main.js
+++ js/main.js
@@ -1,37 +1,36 @@
 // Array with images
 let imgArray = new Array();
 
-    imgArray[0] = new Image();
-    imgArray[0].src = 'img/01.webp';
+    imgArray[0] = 'img/01.webp';
 
-    imgArray[1] = new Image();
-    imgArray[1].src = 'img/02.webp';
+    imgArray[1] = 'img/02.webp';
 
-    imgArray[2] = new Image();
-    imgArray[2].src = 'img/03.webp';
+    imgArray[2] = 'img/03.webp';
 
-    imgArray[3] = new Image();
-    imgArray[3].src = 'img/04.webp';
+    imgArray[3] = 'img/04.webp';
 
-    imgArray[4] = new Image();
-    imgArray[4].src = 'img/05.webp';
+    imgArray[4] = 'img/05.webp';
 
 console.log(imgArray)
 console.log(imgArray.length)
 
 // post array items
 for(let i = 0; i < imgArray.length; i++){
-
+    // List array items
     console.log(imgArray[i]);
 
+    // adding div carousel_item to image_carousel
     var parent = document.getElementById('image_carousel');
     var child = document.createElement("div");
     child.className = "carousel_item";
+
+    // adding img div
+    var imgContainer = document.createElement("img");
+    imgContainer.src = imgArray[i]
+
+    // setting childs
     parent.appendChild(child);
-
-
-    // var createIMG = document.createElement(imgArray[i]);
-    // child.appendChild(createIMG)
+    child.appendChild(imgContainer)
     
 }
 
