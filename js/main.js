034dbbb6fc420c1c39586fd62844afce72ddbb0f
Daviduo <davide0400@gmail.com>
2022-11-12 18:22:26 +0100

    creating array

---
+++ js/main.js
@@ -0,0 +1,20 @@
+// Array with images
+var imgArray = new Array();
+
+    imgArray[0] = new Image();
+    imgArray[0].src = 'img/01.webp';
+
+    imgArray[1] = new Image();
+    imgArray[1].src = 'img/02.webp';
+
+    imgArray[2] = new Image();
+    imgArray[2].src = 'img/03.webp';
+
+    imgArray[3] = new Image();
+    imgArray[3].src = 'img/04.webp';
+
+    imgArray[4] = new Image();
+    imgArray[4].src = 'img/05.webp';
+
+console.log(imgArray)
+console.log(imgArray.length)
